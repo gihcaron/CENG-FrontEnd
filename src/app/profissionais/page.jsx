75446735c51cd6bc34a7877ce0c855f12031d08b
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-09-14 22:10:29 -0300

    Feat: create banner for profissionais page

--- src/app/profissionais/page.jsx
+++ src/app/profissionais/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 import axios from "axios";
 import Image from "next/image";
-import { ToastContainer, toast} from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import { useEffect, useState } from "react";
-import {Pagination, Modal, Card, Skeleton } from "antd";
+import { Pagination, Modal, Card, Skeleton } from "antd";
 import styles from "./styles.module.css";
 
 // Imports
@@ -14,80 +14,111 @@ export default function Profissionais() {
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
-    
- const [data, setData] = useState({
-        profissionais: [], 
-        loading: true,
-        current: 1,
-        pageSize:4
-    });
 
-    useEffect (() => {
-        const fetchProfissionais = async () => {
-            try{
-                const { data : profissionais} = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/profissionais`,
-                );
-                setData({profissionais, loading: false, current: 1, pageSize: 4});
-            }
-            catch(error){
-                console.error("Erro ao buscar profissionais:", error);
-                toast.error("Erro ao buscar profissionais");
-                setData((d) => ({...d, loading: false}));
-            }
-        };
-        fetchProfissionais();
+  const [data, setData] = useState({
+    profissionais: [],
+    loading: true,
+    current: 1,
+    pageSize: 4,
+  });
 
-},[]);
+  useEffect(() => {
+    const fetchProfissionais = async () => {
+      try {
+        const { data: profissionais } = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/profissionais`
+        );
+        setData({ profissionais, loading: false, current: 1, pageSize: 4 });
+      } catch (error) {
+        console.error("Erro ao buscar profissionais:", error);
+        toast.error("Erro ao buscar profissionais");
+        setData((d) => ({ ...d, loading: false }));
+      }
+    };
+    fetchProfissionais();
+  }, []);
 
-const paginatedProfissionais = () => {
+  const paginatedProfissionais = () => {
     const start = (data.current - 1) * data.pageSize;
     return data.profissionais.slice(start, start + data.pageSize);
-};
+  };
 
-    
-    return (
-        <main className={styles.main}>
+  return (
+    <main className={styles.main}>
+      <section className={styles.HeaderSection}>
+        <Header />
+      </section>
 
-        <section className={styles.HeaderSection}>
-            <Header />
-        </section>
-            <h1 className={styles.title}>Profissionais</h1>
+      <section className={styles.bannerSection}>
+        <video className={styles.bannerVideo} autoPlay muted loop playsInline>
+          <source src="/media/carVideo.mp4" type="video/mp4" />
+          Seu navegador não suporta vídeos.
+        </video>
+        <div className={styles.gradientDiagonal}></div>
+      </section>
 
-            <div className={styles.cardsContainer}>
-                {paginatedProfissionais().map((profissional) => (
-<ProfissionalCard
-  key={profissional.id}
-  foto={profissional.foto}
-  alt={profissional.nome}
-  profissional={profissional.nome}
-  area_atuacao={profissional.area_atuacao}
-  categoria_nome={profissional.categoria_nome}
-/>
-                ))}
-            </div>
-                   
-                    <div className={styles.pagination}>
-                        <Pagination
-                            current={data.current}
-                            pageSize={data.pageSize}
-                            total={data.profissionais.length}
-                            onChange={(page) => setData((d) => ({...d, current: page}))}
-                            showSizeChanger={false}
-                        />
-                    </div>
+      <section className={styles.ProfissionalSection}>
+        <div className={styles.bannerContent}>
+        <h1 className={styles.bannerTitle}>
+          Profissionais que brilham no automobilismo!
+        </h1>
+        <p className={styles.bannerSubtitle}>
+          Explore abaixo uma seleção inspiradora de mulheres que se destacam nas mais diversas áreas do setor automobilístico nacional. 
+          Descubra suas trajetórias, conquistas e o impacto que geram no esporte e na indústria. 
+          Navegue pela lista, inspire-se com suas histórias e veja como elas estão abrindo caminhos e acelerando mudanças no Brasil.
+        </p>
+        </div>
+        <div className={styles.ImageBanner}>
+          <Image
+            src="/images/"
+            alt="Mulheres no Automobilismo"
+            width={500}
+            height={300}
+          />
 
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
+
+        </div>
+      </section>
+
+      <section className={styles.profissionaisSection}>
+        <h1 className={styles.title}>Profissionais</h1>
+
+        <div className={styles.cardsContainer}>
+          {paginatedProfissionais().map((profissional) => (
+            <ProfissionalCard
+              key={profissional.id}
+              foto={profissional.foto}
+              alt={profissional.nome}
+              profissional={profissional.nome}
+              area_atuacao={profissional.area_atuacao}
+              categoria_nome={profissional.categoria_nome}
             />
-        </main>
-    );
+          ))}
+        </div>
+
+        <div className={styles.pagination}>
+          <Pagination
+            current={data.current}
+            pageSize={data.pageSize}
+            total={data.profissionais.length}
+            onChange={(page) => setData((d) => ({ ...d, current: page }))}
+            showSizeChanger={false}
+          />
+        </div>
+
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </section>
+    </main>
+  );
 }
